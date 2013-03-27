4c6c11a9019bd6b114f1d2b11e8b8d45bffb280d
Mathilda Thompson <mathildathompson288@Mathildas-MacBook-Pro.local>
2013-03-27 19:14:51 +0000

    Changed questions

--- student.rb
+++ student.rb
@@ -1,19 +1,43 @@
 require 'rubygems'
 require 'yaml'
 
-class Person
+class People
   attr_accessor :name
   attr_accessor :email
   attr_accessor :github_user
   attr_accessor :twitter
   attr_accessor :fun_fact
+
+def self.questions
+  person = People.new
+  print "What is your name? "
+  person.name = gets.strip.chomp
+
+  print "What is your email?"
+  person.email = gets.strip.chomp
+
+  print "What is your github_user?"
+  person.github_user = gets.strip.chomp
+
+  print "What is your twitter?"
+  person.twitter = gets.strip.chomp
+
+  print "What is your fun_fact?"
+  person.fun_fact = gets.strip.chomp
+
+ return person 
 end
 
-class Student < Person
+end
+
+class Student < People
   attr_accessor :reason_for_joining
+  attr_accessor :age
+  attr_accessor :eyecolour
+
 end
 
-class Instructor < Person
+class Instructor < People
   attr_accessor :type
 end
 
@@ -21,25 +45,27 @@ end
 puts "Student Directory, v0.0.1 by Dan Garland"
 print "Enter Student or Instructor, q to save and quit: "
 
+
+
 while ((input = gets.strip.chomp) != 'q') do
 
-  person = nil
+  person = People.questions
   case input
   when 'Student' 
-    person = Student.new
-    print "What is your name? "
-    person.name = gets.strip.chomp
-    print "What is your email? "
-    person.email = gets.strip.chomp
+    # person = Student.new
+    # print "What is your name? "
+    # person.name = gets.strip.chomp
+    # print "What is your email? "
+    # person.email = gets.strip.chomp 
     
   when 'Instructor'
-    person = Instructor.new
-    print "What is your name? "
-    person.name = gets.strip.chomp
-    print "What is your email? "
-    person.email = gets.strip.chomp
-    print "What sort of instructor are you? "
-    person.type = gets.strip.chomp
+    # person = Instructor.new
+    # print "What is your name? "
+    # person.name = gets.strip.chomp
+    # print "What is your email? "
+    # person.email = gets.strip.chomp
+    # print "What sort of instructor are you? "
+    # person.type = gets.strip.chomp
   end
   
   # Append this to our yaml file
